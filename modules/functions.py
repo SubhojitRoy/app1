d406b146accc2f2fa374f97ce34fe1247bf0dd6b
Subhojit Roy <subhojitroy993@gmail.com>
2024-05-12 17:38:37 +0600

    Initial Commit

--- modules/functions.py
+++ modules/functions.py
@@ -17,3 +17,8 @@ Used for writing documentations
 def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
