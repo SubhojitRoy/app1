4b20ae12511938c13c2b958db88c7b8c1f4394d5
Subhojit Roy <subhojitroy993@gmail.com>
2024-05-14 17:51:06 +0600

    Creating file compression software

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,18 +1,29 @@
 import PySimpleGUI as sg
-
+from modules import zip_creator
 
 label_1 = sg.Text("Select file to compress")
 input_1 = sg.Input()
-button_1 = sg.FilesBrowse("Choose")
+choose_button_1 = sg.FilesBrowse("Choose", key="files")
+
 label_2 = sg.Text("Select destination folder")
 input_2 = sg.Input()
-button_2 = sg.FilesBrowse("Choose")
-button_3 = sg.Button("Compress")
+choose_button_2 = sg.FolderBrowse("Choose", key="folder")
 
+compress_button = sg.Button("Compress")
+
+output_label = sg.Text(key="output")
 
 window = sg.Window("File Compresses",
-                   [[label_1, input_1, button_1],
-                    [label_2, input_2, button_2],
-                    [button_3]])
-window.read()
+                   [[label_1, input_1, choose_button_1],
+                    [label_2, input_2, choose_button_2],
+                    [compress_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    zip_creator.make_archive(filepaths, folder)
+    window["output"].update(value="Compression Completed!")
+
 window.close()
--- gui.py
+++ gui.py
@@ -1,17 +1,17 @@
 from modules import functions
 import PySimpleGUI as sg
 
-
 label = sg.Text("Type in To-Do")
 input_box = sg.InputText(tooltip="Enter ToDo", key="todo")
 add_button = sg.Button("Add")
 listbox = sg.Listbox(values=functions.read_todos(), key="todos",
                      enable_events=True, size=(44, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
 window = sg.Window("My To-Do App",
                    [[label], [input_box, add_button],
-                   [listbox, edit_button]],
+                    [listbox, edit_button, complete_button]],
                    font=('Helvetica', 15))
 
 while True:
@@ -34,10 +34,20 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            existing_todos = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.read_todos()
+            index = todos.index(existing_todos)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
+print("Bye")
 window.close()
+
 
 
