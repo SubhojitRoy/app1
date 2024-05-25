54a6ea8798010f883b8ba3144128747cdca40578
Subhojit Roy <subhojitroy993@gmail.com>
2024-05-26 00:19:01 +0600

    Impliment new todo add item feature

--- web.py
+++ web.py
@@ -4,6 +4,12 @@ from modules import functions
 todos = functions.read_todos()
 
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My To-Do App")
 st.subheader("My To-Do App")
 st.write("This is a simple comment")
@@ -11,4 +17,9 @@ st.write("This is a simple comment")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="write your todo...")
+st.text_input(label="", placeholder="write your todo...",
+              on_change=add_todo, key="new_todo")
+
+print("Hello")
+
+st.session_state
